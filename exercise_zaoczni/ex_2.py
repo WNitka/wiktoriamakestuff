fd3d32ab2c31f8032c9f2260375fff56bae33242
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-04-16 17:37:34 +0200

    romb

--- exercise_zaoczni/ex_2.py
+++ exercise_zaoczni/ex_2.py
@@ -9,3 +9,15 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+
+#romb
+
+a1=20
+h1=10
+
+pole_romb = 4*a1
+obwod_romb = a1*h1
+
+print("Obwod rombu wynosi " + str(obwod_romb) + ", zas pole wynisi " + str(pole_romb) + ".")
+
+
