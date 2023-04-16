efb258882a28dd36da902b763fe7a06497e5e12b
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-04-16 16:59:18 +0200

    Changed name

--- exercise_zaoczni/ex_1.py
+++ exercise_zaoczni/ex_1.py
@@ -4,7 +4,7 @@ def hello(a: str) -> str:
 
 
 print("Example")
-print(hello("Wiktoria"))
+print(hello("Wiktoria Nitka"))
 
 assert hello("Ola") == "Hello Ola"
 assert hello("Zuzia") == "Hello Zuzia"
