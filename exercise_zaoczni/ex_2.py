ca2b4dafa526dcb8d9774ac65be29fccf0d56f16
Wiktoria Nitka <wiktoriamnitka.gmail.com>
2023-04-16 17:57:13 +0200

    koło

--- exercise_zaoczni/ex_2.py
+++ exercise_zaoczni/ex_2.py
@@ -1,3 +1,6 @@
+import math
+
+
 # trojkat
 
 a = 10
@@ -19,5 +22,15 @@ pole_romb = 4*a1
 obwod_romb = a1*h1
 
 print("Obwod rombu wynosi " + str(obwod_romb) + ", zas pole wynisi " + str(pole_romb) + ".")
+
+#kolo
+
+pi = math.pi
+r=10
+
+pole_kolo = pi*r**2
+obwod_kolo = pi*r*2
+
+print("Obwod kola wynosi " + str(obwod_kolo) + ", zas pole wynisi " + str(pole_kolo) + ".")
 
 
